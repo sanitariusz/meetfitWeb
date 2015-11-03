9ad87b5f3c9edfa9f1a038fa51ed1652fb518e55
Marcin Bartoszyk <Marcin.Bartoszyk@connectmedica.com>
2015-11-03 16:24:09 +0100

    include frameworks

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express    =    require('express');
 var app        =    express();
 
 require('./router/route')(app);
+app.use(express.static(__dirname + '/public'));
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
--- router/route.js
+++ router/route.js
@@ -1,7 +1,7 @@
 module.exports = function(app)
 {
 	app.get('/',function(req, res){
-		res.render('index.html',{title: "Strona główna"});
+		res.render('index.html',{title: "Strona główna", description: "description of page"});
 	});
 	app.get('/about', function(req,res){
 		res.render('about.html');
