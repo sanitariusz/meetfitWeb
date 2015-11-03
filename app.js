0da71634a29fdf8b0eabc6c06eac6e0c960033c1
Marcin Bartoszyk <Marcin.Bartoszyk@connectmedica.com>
2015-11-03 14:58:54 +0100

    initial commit node

---
+++ app.js
@@ -0,0 +1,11 @@
+var express    =    require('express');
+var app        =    express();
+
+require('./router/route')(app);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server     =    app.listen(3000,function(){
+    console.log("Serwer nasłuchuje na porcie 3000");
+});
