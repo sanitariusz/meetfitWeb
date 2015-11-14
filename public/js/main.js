e13550b0a2c3ef6a2ed1b227ffceac03dbb9f3c7
Marcin Bartoszyk <Marcin.Bartoszyk@connectmedica.com>
2015-11-15 00:01:48 +0100

    menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var main = {
 	init: function(){
-		
+		this._userMenu();
 	},
 	_callAjax: function (url, type, content, data, datatype){
 		console.log(data);
@@ -15,6 +15,20 @@ var main = {
 	        	console.log(data)
 	        }
 	    }); 
+	},
+	_userMenu: function(){
+		$('#user-profile-img').click(function(event) {
+			if($('#notifications-menu').is(':visible')){
+				$('#notifications-menu').hide();
+			}
+			$('#user-menu').toggle();
+		});
+		$('#notifications-img').click(function(event) {
+			if($('#user-menu').is(':visible')){
+				$('#user-menu').hide();
+			}
+			$('#notifications-menu').toggle();
+		});
 	}
 }
 
